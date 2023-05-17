8d87e64ff2f2f5134f180b97b4d7afecf4a573da
ivandlf <98287091+ivandlf@users.noreply.github.com>
2023-05-17 16:02:54 -0300

    añadiendo ejercicios leccion 03

---
+++ Leccion03/ModificadoresAcceso/src/paquete1/Clase1.java
@@ -0,0 +1,13 @@
+package paquete1;
+
+public class Clase1 {
+    public String atributoPublic = "valor atributo publico";
+    
+    public Clase1(){
+        System.out.println("Constructor public");
+    }
+    
+    public void metodoPublico(){
+        System.out.println("Metodo Publico");
+    }
+}
